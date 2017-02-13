100221eb95507937c3bc9aae6e6eefd91d8a28bd
MoraesCaio <MoraesCaio>
2017-02-13 11:04:49 -0200

    Separacao dos metodos de MCH e USR
    MCH: machine's environment variables
    USR: user's environment variables

--- ResetEnvVar/Program.cs
+++ ResetEnvVar/Program.cs
@@ -1,18 +1,17 @@
 using System;
+using System.Diagnostics;
+using System.Deployment.Application;
+using System.Reflection;
 using Microsoft.Win32;
 using System.IO;
-/*using System.Collections.Generic;
-using System.Linq;
-using System.Text;//LOCAL MACHINE x64(semPy)->nenhum;
-using System.Threading.Tasks;
-using System.Security.Permissions;
-using System.Security.Principal;*/
+
 
 namespace ResetEnvVar
 {
     class Program
     {
-        static void apagarVarEnv(string variavel){
+        //MACHINE
+        static void apagarVarEnvMCH(string variavel){
     		try{
 	        	Console.WriteLine("Variável "+variavel+":");
 	        	string t = Environment.GetEnvironmentVariable(variavel,EnvironmentVariableTarget.Machine);
@@ -28,7 +27,7 @@ namespace ResetEnvVar
     		}
         }
 
-        static void criarVarEnv(string variavel, string valor){
+        static void criarVarEnvMCH(string variavel, string valor){
     		try{
 	        	Console.WriteLine("Variável "+variavel+":");
 	        	string t = Environment.GetEnvironmentVariable(variavel,EnvironmentVariableTarget.Machine);
@@ -45,6 +44,7 @@ namespace ResetEnvVar
     		}
         }
 
+        //USER
 		static void apagarVarEnvUSR(string variavel){
     		try{
 	        	Console.WriteLine("Variável "+variavel+":");
@@ -78,115 +78,87 @@ namespace ResetEnvVar
     		}
         }
 
+        //BOTH
+        static void criarVarEnv(string variavel, string valor){
+            criarVarEnvUSR(variavel, valor);
+            criarVarEnvMCH(variavel, valor);
+        }
+
+        static string thisProcessPath(){
+            Process[] proc = Process.GetProcesses();
+            foreach(Process p in proc){
+                if(p.ProcessName.Contains("ResetEnvVar.exe")){
+                    return p.Modules[0].FileName;
+                }
+            }
+            return "";
+        }
         static void Main(string[] args)
         {
-			var lm64 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64);
-            var lm32 = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32);
-        	var cr64 = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry64);
-            var cr32 = RegistryKey.OpenBaseKey(RegistryHive.ClassesRoot, RegistryView.Registry32);
-        	var cu64 = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64);
-            var cu32 = RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32);
-
-
-        	string pathVLibras = Directory.GetCurrentDirectory();
+            //var currentPath = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName;
+            //currentPath = currentPath.Replace("ResetEnvVar.exe","");
+            //var currentPath = Directory +GetParent(thisProcessPath()).FullName, @"VLibras\");
+            string pathVLibras = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName,@"VLibras");// currentPath; //@"..\VLibras";
         	criarVarEnvUSR("PATH_VLIBRAS", pathVLibras);
         	criarVarEnvUSR("AELIUS_DATA", pathVLibras + @"\aelius_data");
         	criarVarEnvUSR("HUNPOS_TAGGER", pathVLibras + @"\bin\hunpos-tag.exe");
         	criarVarEnvUSR("NLTK_DATA", pathVLibras + @"\nltk_data");
         	criarVarEnvUSR("TRANSLATE_DATA", pathVLibras + @"\translate\data");
 
-
-        	//RECUPERAÇÃO DO PYTHON INSTALL PATH PELO REGEDIT
-        	RegistryKey pythonReg = null;
-            //LOCAL MACHINE
-        	if(lm64.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = lm64.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath");
-            }
-            if(lm32.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = lm32.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath");
-            }
-            if(lm32.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = lm32.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath");
-            }
-            if(lm64.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = lm64.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath");
-            }
-            //CURRENT USER
-            if(cu64.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = cu64.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath");
-            }
-            if(cu32.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = cu32.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath");
-            }
-            if(cu32.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = cu32.OpenSubKey(@"SOFTWARE\Python\PythonCore\2.7\InstallPath");
-            }
-            if(cu64.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath") != null){
-                pythonReg = cu64.OpenSubKey(@"SOFTWARE\Wow6432Node\Python\PythonCore\2.7\InstallPath");
-            }
-
-        	if(pythonReg != null){
-        		//CONIFGURAÇÃO DA VARIÁVEL PYTHONPATH
-        		string python = pythonReg.GetValue(null).ToString();
-        		python = python.Remove(python.Length - 1).ToString();
-	            string valor = python+@";"+
-	            python+@"\Scripts;"+
-	            python+@"\Lib\site-packages;"+
-	            pathVLibras+@";"+
-	            pathVLibras+@"\bin;"+
-	            pathVLibras+@"\translate\src;"+
-	            pathVLibras+@"\Clipboard;"+
-	            pathVLibras+@"\update;"+
-	            pathVLibras+@"\nltk_data;"+
-	            pathVLibras+@"\Aelius;";
-	            if(Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Machine) != null){
+       		//CONIFGURAÇÃO DA VARIÁVEL PYTHONPATH
+        	string python = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, @"Python");
+            //@"..\Python";//pythonReg.GetValue(null).ToString();
+            //python = python.Remove(python.Length - 1).ToString();
+            string valor = python+@";"+
+	            python + @"\Scripts;"+
+	            python + @"\Lib\site-packages;"+
+	            pathVLibras + @";"+
+	            pathVLibras + @"\bin;"+
+	            pathVLibras + @"\translate\src;"+
+	            pathVLibras + @"\Clipboard;"+
+	            pathVLibras + @"\update;"+
+	            pathVLibras + @"\nltk_data;"+
+	            pathVLibras + @"\Aelius";
+	            if(Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Machine) != null ||
+                    Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Machine) == ""){
 		            string pythonPathAtual = Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Machine);
-		            if(!pythonPathAtual.Contains(valor)){
-						criarVarEnv("PYTHONPATH", valor+";"+pythonPathAtual);
-		            }
-	            }else{
-	            	criarVarEnv("PYTHONPATH", valor);
+                    if(pythonPathAtual.Contains(python) && pythonPathAtual.Contains(@"\Aelius")){
+                        int comeco = pythonPathAtual.IndexOf(python);
+                        int fim = pythonPathAtual.IndexOf(@"\Aelius");
+                        string substring = pythonPathAtual.Substring(comeco, fim - comeco + (@"\Aelius".Length + 1));
+                        pythonPathAtual = pythonPathAtual.Remove(comeco, substring.Length);
+                    }
+
+                    criarVarEnvMCH("PYTHONPATH", valor+";"+pythonPathAtual);
+
+                }else{
+                    Console.WriteLine("else");
+	            	criarVarEnvMCH("PYTHONPATH", valor);
 	            }
+
 	            string pathPython2 = python+@";"+
-                python+@"\Scripts;";/*Environment.GetEnvironmentVariable("PYTHONPATH", EnvironmentVariableTarget.Machine);
-		    	int idx = pathPython2.IndexOf(@";");
-		    	pathPython2 = pathPython2.Remove(idx, pathPython2.Length - idx);*/
-		    	//pathPython2 = pathPython2 + "\\python.exe";
-	            //CONFIGURAÇÃO DA VARIÁVEL "Path"
+                python +@"\Scripts;";
+
 	        	if(Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine) != null){
 	        		string varPath = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+                    if(varPath.Contains(python) && varPath.Contains(@"\Scripts")){
+                        int comeco = varPath.IndexOf(python);
+                        int fim = varPath.LastIndexOf(@"\Scripts");
+                        string substring = varPath.Substring(comeco, fim - comeco + (@"\Scripts".Length+1));
+                        varPath = varPath.Remove(comeco, substring.Length);
+                    }
 	        		if(!varPath.Contains(pathPython2)){
                         if(varPath.EndsWith(";")){
-                            criarVarEnv("Path", varPath+pathPython2);
+                            criarVarEnvMCH("Path", varPath+pathPython2);
                         }else{
-    	        			criarVarEnv("Path", varPath+";"+pathPython2);
+    	        			criarVarEnvMCH("Path", varPath+";"+pathPython2);
                         }
 	        		}
 	        	}else{
-	        		criarVarEnv("Path", pathPython2);
+	        		criarVarEnvMCH("Path", pathPython2);
 	        	}
-        	}
-    		/*RegistryKey pythonShell = cr64.OpenSubKey(@"Python.File\Shell\open\command");
-            if(pythonShell == null){
-                pythonShell = cr32.OpenSubKey(@"Python.File\Shell\open\command");
-            }
-        	if(pythonShell != null){
-	            //path do python na máquina
-            	string python = pythonShell.GetValue(null).ToString();
-            	//Setando Path
-            	python = python.Remove(python.Length - 9).Replace("\"", "");*/
-
-            	/*string python = @"C:\Python27";
-	        	string valorDePATH = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
-				if(!valorDePATH.Contains(python)){
-					Environment.SetEnvironmentVariable("Path", valorDePATH+@";"+python, EnvironmentVariableTarget.Machine);
-				}*/
-
-            	//python = python.Replace(@"\python.exe","");
-
-
-        	//}
             Console.WriteLine("Variáveis configuradas.");
+            Console.ReadKey();
         }
     }
 }
